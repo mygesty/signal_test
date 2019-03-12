0f52599114f348e3a6f56fe9380b804d17468841
yongbinliang <2300855016@qq.com>
2019-03-12 14:50:56 +0800

    chage to redis

--- signal_test/pipelines.py
+++ signal_test/pipelines.py
@@ -5,17 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymongo
+from redis import StrictRedis
 
 
 class SignalTestPipeline(object):
     def open_spider(self, spider):
-        self.client = pymongo.MongoClient('114.116.122.217', port=27017)
-        self.db = self.client['signal_test']
-        self.collection = self.db['proxies']
+        self.db = StrictRedis(host='114.116.123.62', port=6379, db=0)
+        # self.client = pymongo.MongoClient('114.116.122.217', port=27017)
+        # self.db = self.client['signal_test']
+        # self.collection = self.db['proxies']
 
     def process_item(self, item, spider):
-        self.collection.insert(dict(item))
+        proxy = item['proxy']
+        if not self.db.zscore('proxies', proxy):
+            self.db.zadd('proxies', 100, proxy)
         return item
-
-    def close_spider(self, spider):
-        self.client.close()
